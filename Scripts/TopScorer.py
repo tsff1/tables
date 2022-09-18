c17e2683e7ace4b288da51ca6ac1cd68f9dacae7
Simen Nesland <tsffsome@gmail.com>
2022-09-18 14:45:15 +0200

    topscorer index update + tables

--- Scripts/TopScorer.py
+++ Scripts/TopScorer.py
@@ -27,29 +27,39 @@ def createTopScore(data, avd):
     fontS = ImageFont.truetype("Scripts/Fonts/Aller_Bd.ttf", 25)
     fontB = ImageFont.truetype("Scripts/Fonts/Aller_Bd.ttf", 30)
     y = 230
-    x = 270
+    x = 15
     xOff = [54, 64, 73]
     yOff = [146, 378, 591]
     scale = [200, 181, 163]
 
-    i = 0
+    prevGoals = 0
+    prevNum = 1
     for index, row in data.iterrows():
-        if i < 3:
-            draw.text((x, y),row[0],(0,0,0),font=fontB)
-            draw.text((x + 300, y),row[1],(0,0,0),font=fontB)
-            draw.text((x + 640, y),str(row[2]),(0,0,0),font=fontB)
-            y = y + 222 - i*10
-            logo = Image.open("Scripts/Logoer - Runde/" + row[1] + ".png")
-            size = (scale[i], scale[i])
-            logo = logo.resize(size, Image.ANTIALIAS)
-            img.paste(logo,(xOff[i], yOff[i]), mask = logo)
+
+        if row[2] == prevGoals:
+            num = prevNum
         else:
-            x = 60
-            y = 817 + 64*(i-3)
-            draw.text((x, y),row[0],(0,0,0),font=fontS)
-            draw.text((x + 510, y),row[1],(0,0,0),font=fontS)
-            draw.text((x + 850, y),str(row[2]),(0,0,0),font=fontS)
-        i+=1
+            num = index + 1
+        prevNum = num
+        prevGoals = row[2]
+
+        if index < 3:
+            draw.text((x, y),str(num)+".",(0,0,0),font=fontB)
+            draw.text((x + 255, y),row[0],(0,0,0),font=fontB)
+            draw.text((x + 565, y),row[1],(0,0,0),font=fontB)
+            draw.text((x + 895, y),str(row[2]),(0,0,0),font=fontB)
+            y = y + 222 - index*10
+            logo = Image.open("Scripts/Logoer - Runde/" + row[1] + ".png")
+            size = (scale[index], scale[index])
+            logo = logo.resize(size, Image.ANTIALIAS)
+            img.paste(logo,(xOff[index], yOff[index]), mask = logo)
+        else:
+            x = 15
+            y = 817 + 64*(index-3)
+            draw.text((x, y),str(num)+".",(0,0,0),font=fontS)
+            draw.text((x + 45, y),row[0],(0,0,0),font=fontS)
+            draw.text((x + 555, y),row[1],(0,0,0),font=fontS)
+            draw.text((x + 895, y),str(row[2]),(0,0,0),font=fontS)
 
     avdChr = chr(65 + avd)
     img.save('Scripts/Output/Avd_' + avdChr + '_TS.png')
@@ -58,10 +68,11 @@ def main(avd):
     data = readFromWeb(avd)
     data = shuffle(data)
     data = data.sort_values("Mål", ascending=False)
+    data = data.reset_index(drop=True)
     createTopScore(data, avd)
 
 
 # Velg avdeling: A = 0, B = 1
-avd = 0
+avd = 1
 
 main(avd)
