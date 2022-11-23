78494e38a6f201756dc807d07455733fa07863fe
tsff1 <tsffsome@gmail.com>
2022-11-23 10:07:33 +0100

    Updated table

--- Scripts/main.py
+++ Scripts/main.py
@@ -1,17 +1,30 @@
 from TableGenerator import main
+from git import Repo
 
-# Velg avdeling: A = 0, B = 1
-inp = False
+PATH_OF_GIT_REPO = r"C:\Users\Simen\Tables2\tables"
+COMMIT_MESSAGE = 'Updated table'
 
-while not inp:
+def git_push():
+    repo = Repo(PATH_OF_GIT_REPO)
+    repo.git.add(update=True)
+    repo.index.commit(COMMIT_MESSAGE)
+    origin = repo.remote(name='origin')
+    origin.push()   
+
+end = "n"
+while end.lower() == "n":
     avd = input("What table to update? ")
     if avd.lower() == "a":
-        inp = True
         avd = 0
+        main(avd)
+        print("Table A updated\n")
+        end = input("Done? [y/n] ")
     elif avd.lower() == "b":
-        inp = True
         avd = 1
+        main(avd)
+        print("Table B updated\n")
+        end = input("Done? [y/n] ")
     else:
         print(f"{avd} is not a group\n")
-        
-main(avd)
+
+git_push()
