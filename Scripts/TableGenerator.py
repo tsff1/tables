52904aab62f5207937fdb6b012819f69913a8081
tsff1 <tsffsome@gmail.com>
2023-04-06 12:16:51 +0200

    Updated table

--- Scripts/TableGenerator.py
+++ Scripts/TableGenerator.py
@@ -157,5 +157,6 @@ def main(avd):
     dataList = locateData(df)
     teamResults, teamNames = getData(dataList)
     sortedTeamResults = sortTeams(teamResults, teamNames)
+    printTable(sortedTeamResults, teamNames)
     createTable(sortedTeamResults,teamNames,avd)
     updateTime(avd)
