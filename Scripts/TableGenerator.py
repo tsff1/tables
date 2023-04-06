fd56925c7dac697249fe257145a6ab962e6fc5a1
tsff1 <tsffsome@gmail.com>
2023-04-06 19:48:06 +0200

    Updated table

--- Scripts/TableGenerator.py
+++ Scripts/TableGenerator.py
@@ -35,9 +35,11 @@ def locateData(dataframe):
     for index, row in dataframe.iterrows():
         if str(row[0])[:4] not in ["Week", "nan"]:
             newRow = [row[i] for i in [1,2,4,5]]
-            if isinstance(newRow[1], int):
+            try:
                 newRow[1] = int(newRow[1])
                 newRow[2] = int(newRow[2])
+            except ValueError:
+                pass
             dataList.append(newRow)
     return dataList
 
